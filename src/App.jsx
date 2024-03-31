051bb1e33cd55d4ca740dd1f8acec31a3946e3f5
ShijuPJohn <shijupjohn4u@gmail.com>
2024-04-01 04:26:02 +0530

    question screen, select functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,60 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {fetchUrl} from "./globalConstants.js";
 
 function App() {
+    useEffect(() => {
+        axios.get(`${fetchUrl}/questions?page=1`).then(response => {
+            console.log(response.data.questions)
+            setQuestion(response.data.questions[0])
 
-  return (
-    <>
-        <h1>Helllooooo.......</h1>
-    </>
-  )
+        })
+
+    }, [])
+    const [answered, setAnswered] = useState(false)
+    const [question, setQuestion] = useState({})
+    const [selectedOption, setSelectedOption] = useState([])
+    const [correctAnswers, setCorrectAnswers] = useState([])
+    const [totalScore, setTotalScore]=useState(0)
+    return (
+        <div className="quiz_box">
+            {<h1 className={"quiz_box_quest_title"}>{question.question}</h1>}
+            {question.options && (question.options.map((option, index) => (
+                <h4 className={`quiz_box_option ${selectedOption.includes(index) ? "selected" : undefined} ${correctAnswers.includes(index) ? "right_answer" : undefined}`}
+                    key={index}
+                    style={answered?{cursor:"default"}:{}}
+                    onClick={() => {
+                       if (!answered){
+                           const temp = [...selectedOption]
+                           if (question.questionType==="m-choice") {
+                               setSelectedOption([index])
+                               return
+                           }
+                           if (selectedOption.includes(index)) {
+                               const indexInArray = selectedOption.indexOf(index)
+                               temp.splice(indexInArray, 1)
+                               setSelectedOption([...temp])
+                           } else {
+                               temp.push(index)
+                               setSelectedOption([...temp])
+                           }
+                    }}}
+                >
+                    {option}
+                </h4>
+            )))}
+            <div className="quiz_box_btn_box">
+                <button className="quiz_box_answer_btn" onClick={checkAnswer}>Answer</button>
+            </div>
+        </div>
+    )
+
+    function checkAnswer() {
+        setCorrectAnswers(question.correctOptions)
+        setSelectedOption([])
+        setAnswered(true)
+    }
 }
 
 export default App
---
+++ src/globalConstants.js
@@ -0,0 +1,3 @@
+export const cloudUrl = "https://cloud.google.com/sadfasdfasdfd"
+export const localHostUrl = "http://localhost:8080/api"
+export const fetchUrl = localHostUrl
